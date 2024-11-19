dadce0f063dd28da7d2d6b0536a5a9dbc0b07944
darya-malo <darya-malova@mail.ru>
2024-11-19 19:51:11 +0300

    logs and linters update

--- Python/bug_report_github_issues.py
+++ Python/bug_report_github_issues.py
@@ -1,13 +1,3 @@
-# ---
-# jupyter:
-#   jupytext:
-#     text_representation:
-#       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.16.4
-# ---
-
 """ISSUES."""
 
 # Алгоритм баг-репорта (взаимопомощь):
